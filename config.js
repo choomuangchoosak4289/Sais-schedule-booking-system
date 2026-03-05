9cd78140855df152af5d469ce4502479fc6fbf04
choomuangchoosak4289 <choomuangchoosak4289@gmail.com>
2026-03-05 19:46:27 +0700

    Update SCRIPT_URL in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.SAIS_CONFIG = {
-    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyBIuHb9shBoAFV7WVp_KnlvNTRI4MuiIMfg8Iq8DA8sSrE37hR3L3SFBhDZWR1rCUZ/exec",
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzBovKs5OIANKJk56Rv5rvKz_81JDs2Lf5unk3gGCYAacTa09dmyx5HF_nZJO5lI4TDew/exec",
     ADMIN_USERNAME: "jiraphong2227"
 };
