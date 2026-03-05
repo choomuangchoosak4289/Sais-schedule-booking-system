c147a2e4ace2faf3ebceea83a07049054cd05a28
choomuangchoosak4289 <choomuangchoosak4289@gmail.com>
2026-03-05 19:45:42 +0700

    Add SAIS_CONFIG with SCRIPT_URL and ADMIN_USERNAME

---
+++ config.js
@@ -0,0 +1,4 @@
+window.SAIS_CONFIG = {
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyBIuHb9shBoAFV7WVp_KnlvNTRI4MuiIMfg8Iq8DA8sSrE37hR3L3SFBhDZWR1rCUZ/exec",
+    ADMIN_USERNAME: "jiraphong2227"
+};
