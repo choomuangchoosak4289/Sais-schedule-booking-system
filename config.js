4b5e7862a5e5b2a7840821410ad3c755521c0a7f
choomuangchoosak4289 <choomuangchoosak4289@gmail.com>
2026-03-10 02:20:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 window.SAIS_CONFIG = {
     SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyuT3_uoqSBFsvXodoo8QtNRTUgseB-kEKSWduaJEoUvyn_nmfAGsUo__LF0_HU42Mp-A/exec",
-    ADMIN_USERNAME: "jiraphong2227"
+    ADMIN_USERNAME: "jirapong"
 };
